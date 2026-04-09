705dabbd355a57f1ae3c374c35cfe501541019bd
Santiago Sarmiento <imlunaaa05@gmail.com>
2026-04-08 20:09:45 -0500

    Create videjuego.py

---
+++ Ejercicio 2/videjuego.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+class Entidad(ABC):
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    @abstractmethod
+    def mover(self):
+        pass
+
+class Jugador(Entidad):
+    def mover(self):
+        print(f"{self.nombre} se mueve como jugador")
+
+class Enemigo(Entidad):
+    def mover(self):
+        print(f"{self.nombre} se mueve como enemigo")
+
+class NPC(Entidad):
+    def mover(self):
+        print(f"{self.nombre} se mueve como NPC")
+
+# Polimorfismo
+entidades = [Jugador("Heroe Himmel"), Enemigo("Demonio"), NPC("Aldeano")]
+
+for e in entidades:
+    e.mover()
